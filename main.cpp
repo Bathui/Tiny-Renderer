a8d9ec45fc2f35cf6a90ea98ee4b2db19d0e221a
Bathui <michaelcui1126@gmail.com>
2025-02-23 01:59:26 -0800

    update the z-buffering

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     } 
 }
 
-vec3f baryCentric(vec2i* vertices, vec2i p){
+vec3f baryCentric(vec3f* vertices, vec3f p){
     vec3f coefficient = vec3f(vertices[2].x - vertices[0].x, vertices[1].x - vertices[0].x, vertices[0].x - p.x) ^ vec3f(vertices[2].y - vertices[0].y, vertices[1].y - vertices[0].y, vertices[0].y - p.y);
     
     if(std::abs(coefficient.z) < 1)
@@ -46,27 +46,40 @@ vec3f baryCentric(vec2i* vertices, vec2i p){
     return vec3f(1.f-(coefficient.x+coefficient.y)/coefficient.z, coefficient.y/coefficient.z, coefficient.x/coefficient.z); // return the barycentric coefficient
 }
 
-void triangle(vec2i* pts, TGAImage& img, TGAColor color){
-    vec2i boxmin(img.get_width()-1, img.get_height()-1);
-    vec2i boxmax(0, 0);
+void triangle(vec3f* pts, float* zbuffer, TGAImage& img, TGAColor color){
+    vec2f boxmin( width,  height);
+    vec2f boxmax(0, 0);
 
     for(int i = 0; i < 3; i++){
         //Outer max is to handle the invalid cases
-        boxmin.x = std::max(0, std::min(pts[i].x, boxmin.x));
-        boxmin.y = std::max(0, std::min(pts[i].y, boxmin.y)); 
+        boxmin.x = std::max(0.f, std::min(pts[i].x, boxmin.x));
+        boxmin.y = std::max(0.f, std::min(pts[i].y, boxmin.y)); 
         
-        boxmax.x = std::min(std::max(pts[i].x, boxmax.x), img.get_width()-1);
-        boxmax.y = std::min(std::max(pts[i].y, boxmax.y), img.get_height()-1);
+        boxmax.x = std::min(std::max(pts[i].x, boxmax.x), (float)img.get_width()-1);
+        boxmax.y = std::min(std::max(pts[i].y, boxmax.y), (float)img.get_height()-1);
         
     }
     
-    vec2i inter_points;
+    vec3f inter_points;
     for(inter_points.x = boxmin.x; inter_points.x<= boxmax.x; inter_points.x++) {
         for(inter_points.y = boxmin.y; inter_points.y<= boxmax.y; inter_points.y++){
             vec3f coefficient = baryCentric(pts, inter_points);
             if(coefficient.x < 0 ||coefficient.y < 0|| coefficient.z < 0)
                 continue; //there are some points not in our simplex, so we need to skip those points
-            img.set(inter_points.x, inter_points.y, color);
+            inter_points.z = 0;
+			for(int i = 0; i < 3; i++){
+				if(i==0)
+					inter_points.z += (pts[i].z* coefficient.x);
+				else if(i==1)
+					inter_points.z += (pts[i].z* coefficient.y);
+				else if(i==2)
+					inter_points.z += (pts[i].z* coefficient.z);
+				else;	
+			}
+			if (zbuffer[int(inter_points.x + inter_points.y * width)] < inter_points.z) {
+				zbuffer[int(inter_points.x + inter_points.y * width)] = inter_points.z;
+				img.set(inter_points.x, inter_points.y, color);
+			}
         }
     }
 
@@ -85,56 +98,33 @@ int main(int argc, char** argv) {
 	// Model* model = new Model("sample.obj");
 	// std::cout<<model->num_faces()<<std::endl;
     std::cout<<model->num_verts()<<std::endl;
-
-	// for (int i = 0; i < model->num_faces(); i++) { 
-	// 	std::vector<int> face = model->face(i);
-	// 	// std::cout<<(model->face(0))[6]<<std::endl;
-	// 	for (int j = 0; j < 3; j++) {
-	// 		vec3f v0 = model->vert(face[j*3]);
-	// 		vec3f v1 = model->vert(face[(j+1)%3*3]);
-	// 		int x0 = (v0.X() + 1.) * width/2.;
-	// 		int x1 = (v1.X() + 1.) * width/2.;
-	// 		int y0 = (v0.Y() + 1.) * height/2.;
-	// 		int y1 = (v1.Y() + 1.) * height/2.;
-	// 		line(x0, y0, x1, y1, image, white);
-	// 	}
-	// }
-    
-    // for(int i = 0; i < model->num_faces(); i++){
-    //     std::vector<int> face = model->face(i);
-    //     vec2i screen[3];
-
-    //     for (int j = 0; j < 3; j++) {
-    //         vec3f world = model->vert(face[j*3]);
-    //         screen[j].x = (world.x + 1.) * width / 2.;
-    //         screen[j].y = (world.y + 1.) * height / 2.;
-
-    //     }   
-        
-    //     triangle(screen, image, TGAColor(rand()%256, rand()%256, rand()%256, 255));        
-
-    // }
+	float* zbuffer = new float[width*height];
+	//set default values for z-buffer
+	for (int i=width*height; i>=0; i--)
+		zbuffer[i] = -std::numeric_limits<float>::max();
     vec3f light_direction(0, 0, -1);
-    
+
     for (int i = 0; i < model->num_faces(); i++) {
         vec3f world[3];
-        vec2i screen[3];
+        vec3f screen[3];
         std::vector<int> face = model->face(i);
 
         for(int j = 0; j < 3; j++) {
             world[j] = model->vert(face[3*j]);
-            screen[j] = vec2i((world[j].x + 1.) * width / 2, (world[j].y + 1.) * height / 2);  
+            screen[j] = vec3f(int((world[j].x + 1.) * width / 2 + 0.5), int((world[j].y + 1.) * height / 2+0.5), world[j].Z());  
         }
-        vec3f normal = (world[2] - world[0]) ^ (world[1] - world[0]);
+        
+		vec3f normal = (world[2] - world[0]) ^ (world[1] - world[0]);
         normal.normalized();
         float intensity = normal * light_direction;
 
         if(intensity > 0)
-            triangle(screen, image, TGAColor(255*intensity, 255*intensity, 255*intensity, 255));
+            triangle(screen, zbuffer, image, TGAColor(255*intensity, 255*intensity, 255*intensity, 255));
         
     }
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file(name);
+	delete[] zbuffer;
 	delete model;
 }
 
